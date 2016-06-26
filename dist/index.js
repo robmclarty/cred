09f4f4bc6e10634ec073379d0e32c37255489121
Robert McLarty <rob.m@myplanet.com>
2016-06-26 15:51:48 -0400

    create basic distribution module

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const authentication = require('../src/authentication');
+const authorization = require('../src/authorization');
+
+Object.assign(exports, {
+  authentication,
+  authorization
+});
