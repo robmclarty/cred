c43957b6ac355f2348b8a02840c20c984e36c05f
slode <rob.m@myplanet.com>
2018-04-02 09:42:58 -0400

    simplify main module file in favor of tree-shaking tools to remove unused functions

--- src/index.js
+++ src/index.js
@@ -28,17 +28,11 @@ const cred = ({
     expiresIn: '7 days',
     algorithm: 'HS256'
   }
-}, authorizeOnly = false) => {
+}) => {
   const secretFromOpts = (opts = {}, isPrivate = false) =>
     ['RS256', 'RS384', 'RS512', 'ES256', 'ES384', 'ES512'].includes(opts.algorithm) ?
       (isPrivate ? opts.privateKey : opts.publicKey) :
       opts.secret
-  const requireAccessToken = requireValidToken(
-    key,
-    secretFromOpts(accessOpts),
-    issuer,
-    accessOpts.algorithm
-  )
   const requirePermission = requireResourcePermission(key, resource)
   const requireProp = requirePropIn(key)
 
@@ -51,64 +45,45 @@ const cred = ({
     refreshOpts
   }
 
+  const authentication = initAuthentication({
+    key,
+    issuer,
+    cache,
+    accessOpts: {
+      secret: secretFromOpts(accessOpts, true),
+      expiresIn: accessOpts.expiresIn,
+      algorithm: accessOpts.algorithm
+    },
+    refreshOpts: {
+      secret: secretFromOpts(refreshOpts, true),
+      expiresIn: refreshOpts.expiresIn,
+      algorithm: refreshOpts.algorithm
+    }
+  })
+
+  const requireAccessToken = requireValidToken(
+    key,
+    secretFromOpts(accessOpts),
+    issuer,
+    accessOpts.algorithm
+  )
+
+  const requireRefreshToken = requireValidToken(
+    key,
+    secretFromOpts(refreshOpts),
+    issuer,
+    refreshOpts.algorithm,
+    authentication.verify
+  )
+
   const authorization = {
     requireAccessToken,
+    requireRefreshToken,
     requirePermission,
     requireProp,
     tokenFromReq,
     createError
   }
-
-  const createAuthentication = () => {
-    const auth = initAuthentication({
-      key,
-      issuer,
-      cache,
-      accessOpts: {
-        secret: secretFromOpts(accessOpts, true),
-        expiresIn: accessOpts.expiresIn,
-        algorithm: accessOpts.algorithm
-      },
-      refreshOpts: {
-        secret: secretFromOpts(refreshOpts, true),
-        expiresIn: refreshOpts.expiresIn,
-        algorithm: refreshOpts.algorithm
-      }
-    })
-    const requireRefreshToken = requireValidToken(
-      key,
-      secretFromOpts(refreshOpts),
-      issuer,
-      refreshOpts.algorithm,
-      auth.verify
-    )
-
-    // Add requireRefreshToken() to authorization in the case that both
-    // authorization + authentication are used.
-    Object.assign(authorization, { requireRefreshToken })
-
-    return {
-      use: auth.use,
-      unuse: auth.unuse,
-      authenticate: auth.authenticate,
-      verifyActive: auth.verifyActive,
-      verify: auth.verify,
-      getCache: auth.getCache,
-      revoke: auth.revoke,
-      register: auth.register,
-      refresh: auth.refresh,
-      createToken: auth.createToken,
-      createAccessToken: auth.createAccessToken,
-      createRefreshToken: auth.createRefreshToken
-    }
-  }
-
-  if (authorizeOnly) return Object.assign(settings, authorization)
-
-  // Must createAuthentication() before assigning final return object so that
-  // the authorization object gets updated with requireRefreshToken() which
-  // depends on the authentication instance.
-  const authentication = createAuthentication()
 
   return Object.assign(settings, authentication, authorization)
 }
