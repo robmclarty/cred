718c57cf80fc8b71f98875b76993d8fe80d21175
Rob McLarty <r@robmclarty.com>
2022-11-14 09:44:16 -0800

    refactor: remove /dist

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const authentication = require('../src/authentication');
-const authorization = require('../src/authorization');
-
-Object.assign(exports, authentication, authorization);
